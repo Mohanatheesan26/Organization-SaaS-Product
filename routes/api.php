fb96dacfbc1511659fc091b475332abd341033c3
Mohan <mohanatheesan@gmail.com>
2024-07-19 22:17:06 +0530

    api route updated

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\DeviceController;
-use App\Http\Controllers\LocationController;
-use App\Http\Controllers\OrganizationController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('organizations', OrganizationController::class);
-Route::apiResource('locations', LocationController::class);
-Route::apiResource('devices', DeviceController::class);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
